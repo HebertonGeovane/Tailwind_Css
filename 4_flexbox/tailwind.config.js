311fa1a973397ade82ada063a0fc88257fae5698
Heberton Geovane <heberton.geovane@gmail.com>
2025-01-01 10:49:01 -0300

    feat: add 4_flexbox folder with Flexbox files

---
+++ 4_flexbox/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Isso inclui os arquivos dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
